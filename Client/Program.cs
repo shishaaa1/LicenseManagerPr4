6d7b702b0ceffbb9bbed35405d4a822e36090f39
Kik02 <Golovin.Savelii@yandex.ru>
2025-11-22 16:28:47 +0500

    Разработан метод OnSettings.Гашев

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,11 +1,62 @@
 ﻿using System;
+using System.IO;
+using System.Net;
 namespace Client
 {
     class Program
     {
+        static IPAddress ServerIpAddress;
+        static int ServerPort;
+        static string ClientToken;
+        static DateTime ClientDateConnection;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("");
+            OnSettings();
+        }
+        static void OnSettings()
+        {
+            string Path = Directory.GetCurrentDirectory() + "/.config";
+            string IpAddress = "";
+            if (File.Exists(Path))
+            {
+                StreamReader streamReader = new StreamReader(Path);
+                IpAddress = streamReader.ReadLine();
+                ServerIpAddress = IPAddress.Parse(IpAddress);
+                ServerPort = int.Parse(streamReader.ReadLine());
+                streamReader.Close();
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Server address: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(IpAddress);
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Server port: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(ServerPort.ToString());
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Server address: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                IpAddress = Console.ReadLine();
+                Console.WriteLine(IpAddress);
+
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("Please specify the license server port: ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                ServerPort = int.Parse(Console.ReadLine());
+                StreamWriter streamWriter = new StreamWriter(Path);
+                streamWriter.WriteLine(IpAddress);
+                streamWriter.WriteLine(ServerPort.ToString());
+                streamWriter.Close();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("To change,write the command: ");
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine("/config ");
         }
     }
 }
