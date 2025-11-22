d4a040b562591902f9ddabbaab45d7b81cef6524
Kik02 <Golovin.Savelii@yandex.ru>
2025-11-22 17:20:48 +0500

    29:29 kulikov gashev

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.ComponentModel.Design;
 using System.IO;
 using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
 namespace Client
 {
     class Program
@@ -13,7 +17,96 @@ namespace Client
         static void Main(string[] args)
         {
             OnSettings();
+            while (true)
+                SetCommand();
         }
+
+        public void ConnectServer()
+        {
+            IPEndPoint EndPoint = new IPEndPoint(ServerIpAddress, ServerPort);
+            Socket Socket = new Socket(
+                AddressFamily.InterNetwork,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            try
+            {
+                Socket.Connect(EndPoint);
+            }
+            catch (Exception exp) 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error: " + exp.Message);
+            }
+            if (Socket.Connected)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Connection to server successful");
+
+                Socket.Send(Encoding.UTF8.GetBytes("/token"));
+
+                byte[] Bytes = new byte[10485760];
+                int ByteRec = Socket.Receive(Bytes);
+
+                string Response = Encoding.UTF8.GetString(Bytes,0,ByteRec);
+                if (Response == "/limit")
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("There is not enough space on the license server");
+                }
+                else
+                {
+                    ClientTocken = Response;
+                    ClientDateconnection = DateTime.Now;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Recieved connection token: " + ClientTocken);
+                }
+            }
+
+        static void GetStatus()
+        {
+            int Duration = (int)DateTime.Now.Subtract(ClientDateConnection).TotalSeconds;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine($"Client: {ClientToken}, time connection: {ClientDateConnection.ToString("HH:mm:ss dd.MM")}, " +
+                $"duration: {Duration}");
+
+        }
+
+        static void SetCommand()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            string Command = Console.ReadLine();
+            if (Command == "/config")
+            {
+                File.Delete(Directory.GetCurrentDirectory() + "/.config");
+                OnSettings();
+            }
+            else if (Command == "/connect") ConnectServer();
+            else if (Command == "/status") GetStatus();
+            else if (Command == "/help") Help();
+        }
+
+        static void Help()
+        {
+            Console.ForegroundColor= ConsoleColor.Green;
+            Console.WriteLine("Command to the server: ");
+
+            Console.ForegroundColor=ConsoleColor.Green;
+            Console.Write("/config");
+            Console.ForegroundColor=ConsoleColor.White;
+            Console.WriteLine(" - set initial serrings");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("/connect");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" - connection to the server");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("/status");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" - show list users");
+        }
+
         static void OnSettings()
         {
             string Path = Directory.GetCurrentDirectory() + "/.config";
