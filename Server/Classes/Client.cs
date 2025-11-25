823ff150679a4bf4578f88968cecfaf08a9aa5d4
student-a502 <student-a502@STUD-RM5-A502>
2025-11-25 15:06:11 +0500

    Небольшие исправления

--- Server/Classes/Client.cs
+++ Server/Classes/Client.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Linq;
 
-namespace ConsoleApp1.Classes
+namespace Server.Classes
 {
     class Client
     {
--- Server/Program.cs
+++ Server/Program.cs
@@ -6,7 +6,7 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 
-namespace ConsoleApp1
+namespace Server
 {
     class Program
     {
